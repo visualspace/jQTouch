e94ad6bf7cfa82d790c9e45878122913dbdf95d3
BeeDesk -- Your desk away from your desk <tyip@beedesk.com>
2011-04-18 17:17:33 -0700

    Reverted a change on handleend.

--- jqtouch/jqtouch.js
+++ jqtouch/jqtouch.js
@@ -1363,7 +1363,7 @@
                   }, 1000);
                 } else {
                     if ($marked) $marked.removeClass('active');
-                    //e.preventDefault();
+                    e.preventDefault();
                 }
             };
 
